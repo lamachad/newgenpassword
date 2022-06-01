131f3ebfc4388679bc5a1b4a63778941755f41d7
Leandro <machadoleandro98@gmail.com>
2022-06-01 12:50:46 -0700

    pseudocode added

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,15 @@
+var numbersOption = ["0","1","2","3","4","5","6","7","8","9"];
+var lowerCaseOption = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCaseOption= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var specialCharOption = ["$","^","?",":",",","(",")","{","}","[","]","~","-",".","!","@","#","+","//","/","'"];
+
+// 1.) Prompt user to enter passowrd criteria
+//   a. password length must be between 8 and 128
+//   b. lowercases, uppercases, special characters, and numbers
+// 2.) Validate inpute to ensure one charater type is selected 
+// 3.) diplay generated password
+  
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
